9c7c278d160be4b49335a24f4c70e3bd6d8b11b9
Christian U <christian@ullihome.de>
2022-02-19 06:47:34 +0100

    add docker based sample

--- export.sh
+++ export.sh
@@ -1,1 +1,2 @@
+#docker run --rm -v "$(dirname "$PWD")":/root -w /root/PrimalEarth nytimes/blender:3.0-cpu-ubuntu18.04 blender --background --python export.py
 blender --background --python export.py
