fb92afca2fd2cac5a6903ecce25c5f929c6062d8
Christian U <christian@ullihome.de>
2022-02-22 22:15:08 +0100

    scale plants, fix export

--- export_dev.py
+++
@@ -1,3 +0,0 @@
-import sys,pathlib;sys.path.append(str(pathlib.Path(__file__).parent.parent))
-from blender_export import *
-ExportObject('Dinos/Dimorphodon/Dimorphodon.blend','Dimorphodon','Dinos/Dimorphodon/dimorphodon',lod=[10],export_image_format='JPEG')
